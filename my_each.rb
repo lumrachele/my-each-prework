fce112e6318f4c69bb8f61217c3eb8422fcb7cff
lumrachele <rlum894@gmail.com>
2018-11-16 14:44:23 -0500

    creating an each method using yield

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (words)
+  i=0
+  while i<words.length
+    yield words[i]
+    i+=1
+  end
+  words
 end
+
+
+#test ruby my_each.rb
+#words = ['hi', 'hello', 'bye', 'goodbye']
+#my_each(words) do |item|
+#  puts item
+#end
